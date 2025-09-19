570c9e8f3a4a7f5d72ac1aa7e71309ed51eb2494
hassan1324as <hassan123smn@gmail.com>
2025-09-19 08:11:14 +0300

    added rag with qdrant

--- src/main.py
+++ src/main.py
@@ -1,13 +1,4 @@
 from fastapi import FastAPI
-<<<<<<< HEAD
-from routes  import base,data
-from helpers.config import getSettings
-from motor.motor_asyncio import AsyncIOMotorClient
-from stores.llm.llmProviderFactory import LLMProviderFactory
-app = FastAPI(title="Mini Rag ", version="0.1")
-
-async def startupDbClient():
-=======
 from routes  import base,data , nlp
 from helpers.config import getSettings
 from motor.motor_asyncio import AsyncIOMotorClient
@@ -17,7 +8,6 @@ from stores.llm.templates.templateParser import TemplateParser
 app = FastAPI(title="Mini Rag ", version="0.1")
 @app.on_event("startup")
 async def startupSpan():
->>>>>>> 03fc06c (Initial commit)
     
     settings = getSettings()
     
@@ -26,22 +16,6 @@ async def startupSpan():
     app.db_client = app.mongo_conn[settings.MONGODB_DATABASE]
     
     llmProviderFactory = LLMProviderFactory(settings)
-<<<<<<< HEAD
-    app.generationClient = llmProviderFactory.create(provider=settings.GENERATION_BACKEND)
-    app.generationClient.setGenerationModel(modelId=settings.GENERATION_MODEL_ID)
-    app.embeddingClient = llmProviderFactory.create(provider=settings.EMBEDDING_BACKEND)
-    app.embeddingClient.setEmbeddingModel(modelId=settings.EMBEDDING_MODEL_ID)
-    app.embeddingClient.setEmbeddingModel(modelId=settings.EMBEDDING_MODEL_SIZE)
-
-async def shutDown():
-    app.mongo_conn.close()
-
-app.router.lifespan.on_startup.append(startupDbClient)
-app.router.lifespan.on_shutdown.append(shutDown)
-
-app.include_router(base.baseRouter)
-app.include_router(data.dataRouter)
-=======
     vectorDBProviderFactory = VectorDBProviderFactory(settings) 
 
 
@@ -68,5 +42,4 @@ async def shutDownSpan():
 app.include_router(base.baseRouter)
 app.include_router(data.dataRouter)
 app.include_router(nlp.nlpRouter)
->>>>>>> 03fc06c (Initial commit)
 
